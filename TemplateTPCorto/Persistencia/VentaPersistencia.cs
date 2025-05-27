10717e958ea8db61694e89aa7f5df33d5cd62867
Juani <juanigeiler2001@gmail.com>
2025-05-26 23:44:21 -0300

    componentes de fase 2 del repo base agregados

---
+++ TemplateTPCorto/Persistencia/VentaPersistencia.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Persistencia.WebService.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistencia
+{
+    public class VentaPersistencia
+    {
+        private Guid idUsuario = new Guid("784c07f2-2b26-4973-9235-4064e94832b5");
+
+        /*
+        public bool agregarVenta(venta)
+        {
+            var jsonRequest = JsonConvert.SerializeObject(venta);
+
+            HttpResponseMessage response = WebHelper.Post("Venta/AgregarVenta", jsonRequest);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return true;
+            }
+
+            return false;
+        }
+        */
+    }
+}
