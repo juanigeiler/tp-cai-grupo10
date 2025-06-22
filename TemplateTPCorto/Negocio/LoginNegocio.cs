562d28f0f469f7b77cbf5f3be93d88dc7ccc12e1
Juani <juanigeiler2001@gmail.com>
2025-06-22 12:59:50 -0300

    fix en nombre de metodo

--- TemplateTPCorto/Negocio/LoginNegocio.cs
+++ TemplateTPCorto/Negocio/LoginNegocio.cs
@@ -121,7 +121,7 @@ namespace Negocio
         public void CambiarPasswordPrimerLogin(string legajo, string nuevaPassword)
         {
             UsuarioPersistencia usuarioPersistencia = new UsuarioPersistencia();
-            usuarioPersistencia.ActualizarPassword(legajo, nuevaPassword); 
+            usuarioPersistencia.ActualizarContraseña(legajo, nuevaPassword); 
             usuarioPersistencia.ActualizarFechaUltimoLogin(legajo, DateTime.Now);
         }
     }
